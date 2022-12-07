97241e68a1bf6b60030613092a220a16ff6d8c78
kadirula <kadirula17@gmail.com>
2022-12-07 12:29:45 +0300

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useEffect, useRef } from 'react';
-import { RouterProvider, useNavigate } from 'react-router-dom'
-import { fetchFromAPI } from './api/fetch';
+import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router';
-import { useDispatch, useSelector } from 'react-redux';
-import { setGenres } from './redux/reducers/genresReducer'
 import VideoCard from './components/VideoCard';
 import ModalContent from './components/modal/ModalContent';
-import { setError, setSearchStatus } from './redux/reducers/siteReducer';
+import { useSelector } from 'react-redux';
 
 const App = () => {
 
   const { movieModal } = useSelector(state => state.modal);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  // Film Türlerini Yakalar
-  useEffect(() => {
-    fetchFromAPI(`genre/movie/list`).then(res => {
-      if (res.status === 200) {
-        dispatch(setGenres(res.data.genres));
-      }
-      else {
-        dispatch(setError(res))
-        navigate('/error');
-      }
-    })
-  }, [])
-
-
-
-
 
   return (
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchFromAPI } from '../api/fetch';
 import MovieBanner from '../components/section/MovieBanner';
 import NowPlayingMovie from '../components/section/NowPlayingMovie';
 import PopularMovie from '../components/section/PopularMovie';
 import TopRatedMovie from '../components/section/TopRatedMovie';
 import Slider from '../components/sliders/Slider';
+import { setGenres } from '../redux/reducers/genresReducer';
+import { setError } from '../redux/reducers/siteReducer';
 
 
 
 const Home = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // Film Türlerini Yakalar
+  useEffect(() => {
+    fetchFromAPI(`genre/movie/list`).then(res => {
+      if (res.status === 200) {
+        dispatch(setGenres(res.data.genres));
+      }
+      else {
+        dispatch(setError(res))
+        navigate('/error');
+      }
+    })
+  }, [])
+
   return (
     <>
       <Slider />
-      
+
       <section className="section">
         <TopRatedMovie />
       </section>
@@ -29,7 +52,7 @@ const Home = () => {
       </section>
 
       <section className="section">
-        <NowPlayingMovie/>
+        <NowPlayingMovie />
       </section>
     </>
   )
